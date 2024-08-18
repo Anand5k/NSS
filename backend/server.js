1f0f095f41d28a1f9fed83f32947824bf8fa09f2
Anand5k <169077283+Anand5k@users.noreply.github.com>
2024-08-18 19:20:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,7 +51,9 @@ db.connect(); */
   ConnectionString: postgresql://NSS_owner:OZ1FfA7XRnkr@ep-bold-haze-a14k7ohz.ap-southeast-1.aws.neon.tech/NSS?sslmode=require
 }); */
 
-
+app.get('/',async (req,res)=>{
+  return res.status(201).json({ msg: 'OK' });
+})
 
 app.post('/signup', async (req, res) => {
   try {
